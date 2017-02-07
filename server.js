6829da0951f7f5fb1e56ee5bd6b45499688fb299
mritunjaysri <mritunjay.nptel@gmail.com>
2017-02-07 07:40:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article_one', function(req, res){
-    res.send("Web page linked at server side");
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -21,6 +17,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/article_one', function(req, res){
+    res.send("Web page linked at server side");
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80

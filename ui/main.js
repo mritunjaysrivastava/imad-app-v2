2c14a17c5621c986bb9cf3216083b3e3cf8a7b5e
mritunjaysri <mritunjay.nptel@gmail.com>
2017-02-12 22:04:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,8 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "DEVIL!";
 
+//move text
+var img = document.getElementById("madi");
+img.onclick = function() {
+    img.style.marginleft='100px';
+} 

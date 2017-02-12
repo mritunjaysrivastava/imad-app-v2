d01445891b304d944e72e6ddab7ba155e08d61e7
mritunjaysri <mritunjay.nptel@gmail.com>
2017-02-12 21:48:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/', function (req, res) {
 });
 
 //style.css
-app.get('/', function (req, res) {
+app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 

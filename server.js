6b5b570666bf0788365af59f952e64fd5aa9c913
mritunjaysri <mritunjay.nptel@gmail.com>
2017-02-12 20:27:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,6 +83,11 @@ app.get('/mergejshtml', function (req, res) {
   res.send(html(merge));
 });
 
+//main.js
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 //set port
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {

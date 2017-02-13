c019fe3a830a57b8a49be79e4f6fd547e9f18a84
mritunjaysri <mritunjay.nptel@gmail.com>
2017-02-13 23:19:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ button.onclick =function () {
     
     //capture the request
     request.onreadystatechange = function () {
-      if(request.reayState === XMLHttpRequest.DONE) {
+      if(request.readyState === XMLHttpRequest.DONE) {
           
           //action perfromed to respond or render a respond
           if(request.status === 200) {

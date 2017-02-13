da86d77b4c19bd5f557260d665d9360154df8cb9
mritunjaysri <mritunjay.nptel@gmail.com>
2017-02-13 23:21:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,7 +22,7 @@ var button = document.getElementById("counter");
 
 button.onclick =function () {
     //Create a request object
-    var request = XMLHttpRequest();
+    var request = new XMLHttpRequest();
     
     //capture the request
     request.onreadystatechange = function () {

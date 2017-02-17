15c9380a7e6bf60b055997febf674538c90a10a2
mritunjaysri <mritunjay.nptel@gmail.com>
2017-02-17 08:49:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,29 +16,3 @@ function moveRight () {
 img.onclick = function() {
     var interval = setInterval(moveRight, 50);
 }; 
-
-
-//counter per click
-var button = document.getElementById("counter");
-
-button.onclick =function () {
-    //Create a request object
-    var request = new XMLHttpRequest();
-    
-    //capture the request
-    request.onreadystatechange = function () {
-      if(request.readyState === XMLHttpRequest.DONE) {
-          
-          //action perfromed to respond or render a respond
-          if(request.status === 200) {
-              var res = request.respondText;
-              var respond = document.getElementById("count_like");
-              respond.innerHTML = res.toString();
-          }
-      }
-    };
-    
-    //Make a request
-    request.open('GET', 'http://mritunjaysri.imad.hausra-app.io/counter', true);
-    request.send(null);
-};

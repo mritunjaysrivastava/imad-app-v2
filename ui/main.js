8271bc2c7bdf50d2e20bf6bdc273c98a63e576d5
mritunjaysri <mritunjay.nptel@gmail.com>
2017-03-14 18:50:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,16 +12,15 @@ button.onclick = function () {
     request.onreadystateChange = function () {
         if(request.readyState === XMLHttpRequest.DONE) {
             if(request.status === 200) {
-                var counter = request.responseText;
+                var counte = request.responseText;
                 var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+                span.innerHTML = counte.toString();
             }
         }
     };
     
     request.open('GET',"http://mritunjaysri.imad.hasura-app.io/counter",true);
     request.send(null);
-    alert(counter.toString());
 };
 
 //count like

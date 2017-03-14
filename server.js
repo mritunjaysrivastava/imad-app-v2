94ef6f32cebfd1bf2e4334d5951f01bc57f9a775
mritunjaysri <mritunjay.nptel@gmail.com>
2017-03-14 10:47:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var Pool = require('pg').Pool;  //import db-connection
 var config = {
     host: 'db.imad.hausra-app.io',
     user: 'mritunjaysri',
-    password: process.emv.DB.PASSWORD,
+    password: 'db-mritunjaysri-34582',
     database: 'mritunjaysri',
     port: '5432'
 };

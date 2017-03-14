cb89e47e00dd0d9ee05192218e9d7e31e432e4de
mritunjaysri <mritunjay.nptel@gmail.com>
2017-03-14 18:14:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ button.onclick = function () {
 };
 
 //count visit
-var button = document.getElementById("vcounter");
+var button = document.getElementById("counter");
 var counter = 0;
 button.onclick = function () {
     var request = new XMLHttpRequest();
@@ -23,7 +23,7 @@ button.onclick = function () {
         if(request.readyState === XMLHttpRequest.DONE) {
             if(request.status === 200) {
                 var counter = request.responseText;
-                var span = document.getElementById('vcount');
+                var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
             }
         }

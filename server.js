59b63928eddb722030dccc5b50fe010a5f036e53
MRITUNJAY SRIVASTAVA <mritunjay.nptel@gmail.com>
2017-04-05 12:40:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,10 +115,14 @@ app.get('/articles/:articleName', function (req, res) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
-          res.send(JSON.stringify(result.rows));               //return with response with result
+          if (result.rows.length === 0) {
+              res.status(404).send('Article not found');
+          } else {
+              var articleData = result.rows[0];
+              res.send(createTemplate(articleData));
+          }
       }
    });
-    res.send(createTemplate(articlesData));
 });
 
 //counter

23d468b239d6b7be0e337e950f324debadc99fef
MRITUNJAY SRIVASTAVA <mritunjay.nptel@gmail.com>
2017-04-05 14:35:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ function createTemplate (data) {
                     ${title}
                 </div>
                 <div>
-                    ${date}
+                    ${date.toDateString()}
                 </div>
                 <div>
                   ${content}
@@ -111,7 +111,7 @@ app.get('/:articleName', function (req, res) {
 
 //db content
 app.get('/articles/:articleName', function (req, res) {
-    pool.query("SELECT * FROM article where title = '" + req.params.articleName + "'", function(err, result){       //make a request
+    pool.query("SELECT * FROM article where title = $1'" + [req.params.articleName] + "'", function(err, result){       //make a request
       if (err) {
           res.status(500).send(err.toString());
       } else {
